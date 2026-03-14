9d78c8ca87ff893a00816f7c8800f70ff15823ce
NorthStar Home Technologies <office@northstarhometech.com>
2026-03-14 17:24:54 +0000

    Wire GTM via NEXT_PUBLIC_GTM_ID env var (GTM-W99LHKT)
    - Replace hardcoded GTM-XXXXXXX placeholder with env var
    - GTM script only loads when env var is set (no broken tags in dev)
    - Conditional noscript fallback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,27 +43,30 @@ export default function RootLayout({
     <html lang="en" className={`${forum.variable} ${nunitoSans.variable}`}>
       <head>
         {/* Google Tag Manager */}
-        {/* TODO: Replace GTM-XXXXXXX with actual GTM container ID */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-XXXXXXX');`,
-          }}
-        />
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`,
+            }}
+          />
+        )}
       </head>
       <body className="font-body antialiased">
         {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
         <Navigation />
         <main>{children}</main>
         <Footer />
